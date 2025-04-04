d7314e9b058034cd9a44c5eed5f7366b95da13b3
Birnarsa-Uz <saidmurodziynatov0@gmail.com>
2025-04-04 14:20:06 +0500

    add news detail

--- news/views.py
+++ news/views.py
@@ -10,7 +10,6 @@ class HomePageView(ListView):
     template_name = 'home.html'
     context_object_name = 'categories'
     
-    
     def get_queryset(self):
         categories = Category.objects.all()
         data = []
@@ -34,12 +33,23 @@ class CategoryPageListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['category'] = Category.objects.get(slug=self.kwargs['slug'])
-        context['categories'] = Category.objects.all()
         return context
 
 class NewsDetailView(DetailView):  
     model = Post
     template_name = 'news_detail.html'  
+    context_object_name = 'post'    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        categories = Category.objects.all()
+        data = []
+        for category in categories:
+            posts = Post.objects.filter(category=category).all()
+            data.append({'category': category, 'post_len': len(posts)})
+        context['post_lens'] = data
+        return context
 
 class LoginPageView(TemplateView):
     template_name = 'login.html'   
